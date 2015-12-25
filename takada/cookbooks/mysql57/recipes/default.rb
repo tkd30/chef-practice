d22e3d7a2a01c0b140c410bfdc4937d2d70c8ce5
Mio Takada <takada_mio@cyberagent.co.jp>
2015-12-25 23:01:09 +0900

    ちょっとこの表記～でコメントにしてたリソースを削除

--- takada/cookbooks/mysql57/recipes/default.rb
+++ takada/cookbooks/mysql57/recipes/default.rb
@@ -45,23 +45,6 @@ template "/etc/my.cnf" do
 	notifies :run, 'script[set_root_password]', :delayed
 end
 
-# ちょっとこの表記一旦保留
-#yum_repository 'mysql57-community' do
-#  description "MySQL 5.7 Community Server"
-#  baseurl node[:mysql][:mysql75_url]
-#  enabled false
-#  gpgcheck false
-#  sslverify false
-#  timeout '10'
-#  action :create
-#end
-#
-#package "mysql-community-server" do
-##  version "#{node[:mysql][:version]}"
-#  action :install
-##  options "--enablerepo=mysql57-community"
-#end
-
 service "mysqld" do
   action [ :start, :enable ]
 end
